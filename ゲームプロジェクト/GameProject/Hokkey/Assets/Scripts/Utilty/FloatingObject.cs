02408cac5ea8a8e242dd5665993175434cda2bcc
sakamotokun <sw.style0312@gmail.com>
2015-12-02 17:57:54 +0900

    タイトル画面作成

---
+++ ゲームプロジェクト/GameProject/Hokkey/Assets/Scripts/Utilty/FloatingObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatingObject : MonoBehaviour 
+{
+	public Transform myTrans;
+	private float ypos;
+	private bool isActive;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Active();
+		ypos=myTrans.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(isActive)
+		{
+			ypos = Mathf.Sin (Time.frameCount / 10.0f);
+			ypos *= 0.01f;
+			Vector3 vector = new Vector3(0.0f,ypos,0.0f);
+			myTrans.position += vector;
+		}
+	}
+
+	public void Active(){isActive=true;}
+	public void UnActive(){isActive=false;}
+
+}
